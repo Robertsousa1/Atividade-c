11383d95469cee68054a4965ec9c404cea62b46d
Robertsousa1 <robertsmiranda25@gmail.com>
2025-05-13 20:14:34 -0300

    Create Questão30.c

---
+++ PUCRS/Questão30.c
@@ -0,0 +1,12 @@
+// Questão 30
+#include <stdio.h>
+
+int main() {
+    printf("Números ímpares entre 100 e 200:\n");
+
+    for(int i = 101; i < 200; i += 2) {
+        printf("%d\n", i); // Exibe os números ímpares
+    }
+
+    return 0;
+}

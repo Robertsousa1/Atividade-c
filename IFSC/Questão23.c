43ab0e2a9a8617660745fde25552251767a3cc98
Robertsousa1 <robertsmiranda25@gmail.com>
2025-05-13 23:25:46 -0300

    Create Questão23.c

---
+++ IFSC/Questão23.c
@@ -0,0 +1,18 @@
+// Questão 23
+
+#include <stdio.h>
+
+int main() {
+    int numA, numB;
+
+    // Leitura dos valores NumA e NumB
+    printf("Digite o valor de A: ");
+    scanf("%d", &numA);
+    printf("Digite o valor de B: ");
+    scanf("%d", &numB);
+
+    // Exibição dos valores em ordem inversa
+    printf("Os valores em ordem inversa são: %d e %d\n", numB, numA);
+
+    return 0;
+}

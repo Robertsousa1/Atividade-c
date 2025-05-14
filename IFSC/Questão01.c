779cd6449e3b6b46bbddc2adbef1308b69c3c7c9
Robertsousa1 <robertsmiranda25@gmail.com>
2025-05-13 22:58:21 -0300

    Create Questão01.c

---
+++ IFSC/Questão01.c
@@ -0,0 +1,13 @@
+a) (20 - 15)/2 = 2.5 → real
+b) 20 - 15/2 = 12.5 → real
+c) 25/20 + 30/152 = 4.5 → real
+d) 2*(5/20) + 30/(15*2) = 1.5 → real
+e) 23 div 4 = 5 → inteiro
+f) 23 mod 4 = 3 → inteiro
+g) 35 div 6 + 2 = 7 → inteiro
+h) 35 div 6 - 2 = 3 → inteiro
+i) 35 div 6 * 2 = 10 → inteiro
+j) sqrt(625) = 25 → inteiro
+k) sqr(20) = 400 → inteiro
+l) 2 + sqrt(21 div 5) = 4 → inteiro
+

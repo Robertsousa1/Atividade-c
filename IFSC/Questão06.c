a4a0685007079c7b5288e1bf396ab59aa9af8611
Robertsousa1 <robertsmiranda25@gmail.com>
2025-05-13 23:09:53 -0300

    Create Questão06.c

---
+++ IFSC/Questão06.c
@@ -0,0 +1,6 @@
+( ) teste = cod OU ((x)*2 <> soma) — Inválido
+(X) tudo = soma — Inválido devido a incompatibilidade de tipos
+( ) x = nome >= cor — Inválido
+(X) cod = cor = 'verde' — Válido
+( ) tudo = NÃOtesteOUcodE(soma<x) — Inválido
+

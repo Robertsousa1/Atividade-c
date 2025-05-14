2cae2e61f60054ed0f1d319bb73616d7b27f9063
Robertsousa1 <robertsmiranda25@gmail.com>
2025-05-13 23:15:22 -0300

    Create Questão10.c

---
+++ IFSC/Questão10.c
@@ -0,0 +1,21 @@
+// Questão 10
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, soma;
+
+    // Leitura dos dois números inteiros
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    // Cálculo da soma dos dois números
+    soma = num1 + num2;
+
+    // Exibição do resultado da soma
+    printf("A soma é: %d\n", soma);
+
+    return 0;
+}

49cf262dbe692eb61118de639dbee98cf7d72a99
Robertsousa1 <robertsmiranda25@gmail.com>
2025-05-14 00:35:25 -0300

    Create Questão90.c

---
+++ IFSC/Questão90.c
@@ -0,0 +1,13 @@
+// Questão 90 
+
+#include <stdio.h>
+
+int main() {
+    // Imprime múltiplos de 7 menores que 1000
+    for (int i = 7; i < 1000; i += 7) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return 0;
+}
